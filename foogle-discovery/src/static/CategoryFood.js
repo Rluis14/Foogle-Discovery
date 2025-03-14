20af75049001c37fdbf080147d7c4991e36af1fb
Quan Giap <minhquangiapnguyen@gmail.com>
2025-03-13 19:37:48 -0700

    add select category input

---
+++ foogle-discovery/src/static/CategoryFood.js
@@ -0,0 +1,2 @@
+const categoryfood = ["Beef", "Breakfast", "Chicken", "Dessert", "Goat", "Lamb", "Miscellaneous", "Pasta", "Pork", "Seafood", "Side", "Starter", "Vegan", "Vegetarian"];
+export default categoryfood;
